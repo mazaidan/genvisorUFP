ec9acb6572fa2ad871e5b20ac37559da1740a6c5
Zaidan Martha A B <martha_arbayani@yahoo.com>
2021-07-21 05:54:24 +0300

    only code for modelling

---
+++ LCS_TSI_modelling.m
@@ -0,0 +1,11 @@
+%% This code is written in MATLAB
+% The purpose aims to develop the estimator for ultra-fine particles based
+% on low-cost sensors
+% Written by:
+% Martha Arbayani Bin Zaidan, Ph.D., Docent, CEng.
+% Research Associate Professor, Nanjing University, China
+% Senior Scientist, Helsinki University, Finland
+clear all;close all;clc;
+
+load('DATA2.mat') 
+DATA = DATA2;
